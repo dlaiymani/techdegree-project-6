af81d682a3a7cabc2d6e1b6fc60c5baf43757d62
David laiymani <david.laiymani@gmail.com>
2019-05-01 11:39:34 +0200

    Refactoring + metric conversion + description labels

--- AwakensApp/AwakensApp/Controllers/AwakenController.swift
+++ AwakensApp/AwakensApp/Controllers/AwakenController.swift
@@ -22,20 +22,18 @@ class AwakenController: UITableViewController, UIPickerViewDataSource, UIPickerV
     @IBOutlet weak var smallestLabel: UILabel!
     @IBOutlet weak var largestLabel: UILabel!
     @IBOutlet weak var unitConverter: UISegmentedControl!
+    @IBOutlet weak var currencyConverter: UISegmentedControl!
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     
     @IBOutlet var descriptionLabels: [UILabel]!
     
-    
-    
+    let descriptionLabelsForPeople = ["Born", "Home", "Height", "Eyes", "Hairs"]
+    let descriptionLabelsForVehicle = ["Make", "Cost", "Length", "Class", "Crew"]
     
     let client = AwakensAPIClient()
-    var peopleData = [People]()
-    var vehicleData = [Vehicle]()
-    var starshipData = [Starship]()
-    var transportMachineDate = [TransportMachine]()
+    
     var data = [AwakenData]()
     var currentPeople: People?
     var currentVehicle: Vehicle?
@@ -47,10 +45,19 @@ class AwakenController: UITableViewController, UIPickerViewDataSource, UIPickerV
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureDecriptionLabels(forEntity: entity!)
         dataListPickerView.delegate = self
         dataListPickerView.dataSource = self
         
         unitConverter.selectedSegmentIndex = 1
+        currencyConverter.selectedSegmentIndex = 1
+        if let entity = entity {
+            if entity == .people {
+                currencyConverter.isHidden = true
+            } else {
+                currencyConverter.isHidden = false
+            }
+        }
         activityIndicator.startAnimating()
         
         client.searchForData(with : endpoint!, forEntity: entity!) { [weak self] data, nb, error in
@@ -58,87 +65,21 @@ class AwakenController: UITableViewController, UIPickerViewDataSource, UIPickerV
             self?.createDataArray(with: data, forSize: nb!)
         }
         
-      /*  if let entity = entity {
-            downloadData(for: entity)
-        }*/
-        
-        
-      /*  client.searchForCharacters { [weak self] peoples, nb, error in
-            self?.peopleData.append(contentsOf: peoples)
-            if (self?.peopleData.count)! >= nb! { // all data are downloaded
-                self?.peopleData.sort(by: { $0.name < $1.name })
-                self?.dataListPickerView.reloadAllComponents()
-                self?.activityIndicator.stopAnimating()
-                self?.activityIndicator.isHidden = true
-                let smallest = self?.peopleData.min(by: { $0.height < $1.height })
-                let largest = self?.peopleData.max(by: { $0.height < $1.height })
-                self?.smallestLabel.text = smallest?.name
-                self?.largestLabel.text = largest?.name
-                self?.pickerView(self!.dataListPickerView, didSelectRow: 0, inComponent: 0)
+    }
+    
+    
+    func configureDecriptionLabels(forEntity entity: Entity) {
+        for (index, _) in descriptionLabels.enumerated() {
+            if entity == .people {
+                self.descriptionLabels[index].text = descriptionLabelsForPeople[index]
+            } else {
+                descriptionLabels[index].text = descriptionLabelsForVehicle[index]
+
             }
-        }*/
-    
+        }
     }
     
-    func downloadData(for entity: Entity) {
-        switch entity {
-        case .people:
-            searchForPeople()
-            print(endpoint!.request)
-        case .vehicles:
-            searchForVehicle()
-            print(endpoint!.request)
 
-        case .starships:
-            //searchForData(in: starshipData)
-            print(endpoint!.request)
-        default:
-            break
-        }
-    }
-    
-    
-    func searchForPeople() {
-        client.searchForData(with : endpoint!, forEntity: .people) { [weak self] peoples, nb, error in
-            
-            self?.createDataArray(with: peoples, forSize: nb!)
-            
-//            self?.data.append(contentsOf: peoples)
-//            //self?.peopleData.append(contentsOf: peoples)
-//            if (self?.data.count)! >= nb! { // all data are downloaded
-//                self?.data.sort(by: { $0.name < $1.name })
-//                self?.dataListPickerView.reloadAllComponents()
-//                self?.activityIndicator.stopAnimating()
-//                self?.activityIndicator.isHidden = true
-//             //   let smallest = self?.data.min(by: { $0.height < $1.height })
-//             //   let largest = self?.data.max(by: { $0.height < $1.height })
-//               // self?.smallestLabel.text = smallest?.name
-//             //   self?.largestLabel.text = largest?.name
-//                self?.pickerView(self!.dataListPickerView, didSelectRow: 0, inComponent: 0)
-//            }
-        }
-    }
-    
-    func searchForVehicle() {
-        client.searchForVehicles(with : endpoint!) { [weak self] vehicles, nb, error in
-            
-            self?.createDataArray(with: vehicles, forSize: nb!)
-
-           /* self?.vehicleData.append(contentsOf: vehicles)
-            if (self?.vehicleData.count)! >= nb! { // all data are downloaded
-                self?.vehicleData.sort(by: { $0.name < $1.name })
-                self?.dataListPickerView.reloadAllComponents()
-                self?.activityIndicator.stopAnimating()
-                self?.activityIndicator.isHidden = true
-                let smallest = self?.vehicleData.min(by: { $0.length < $1.length })
-                let largest = self?.vehicleData.max(by: { $0.length < $1.length })
-                self?.smallestLabel.text = smallest?.name
-                self?.largestLabel.text = largest?.name
-                self?.pickerView(self!.dataListPickerView, didSelectRow: 0, inComponent: 0)
-            }*/
-        }
-    }
-    
     
     func createDataArray(with awakenData: [AwakenData], forSize size: Int) {
         self.data.append(contentsOf: awakenData)
@@ -197,7 +138,7 @@ class AwakenController: UITableViewController, UIPickerViewDataSource, UIPickerV
         self.bornYearLabel.text = viewModel.birthYear
         self.eyeColorLabel.text = viewModel.eyeColor
         self.hairColorLabel.text = viewModel.hairColor
-        self.displayHeight(height: viewModel.height)
+        self.displayMeasure(measure: viewModel.height)
         client.lookupHome(withId: viewModel.home) { (home, error) in
             self.homeLabel.text = home
         }
@@ -209,24 +150,41 @@ class AwakenController: UITableViewController, UIPickerViewDataSource, UIPickerV
         self.eyeColorLabel.text = viewModel.cost
         self.hairColorLabel.text = viewModel.crew
         self.homeLabel.text = viewModel.crew
-        self.displayHeight(height: viewModel.length)
+        self.displayMeasure(measure: viewModel.length)
     }
     
     
     @IBAction func unitChanged(_ sender: Any) {
         metricUnit = !metricUnit
         if let people = currentPeople {
-            displayHeight(height: people.measure)
+            displayMeasure(measure: people.measure)
+        }
+        if let vehicle = currentVehicle {
+            displayMeasure(measure: vehicle.measure)
         }
     }
     
-    func displayHeight(height: String) {
-        if metricUnit {
-            self.heightLabel.text = "\(height)m"
-        } else {
-            let heightInFeet = Double(height)!*0.0328084
-            let formattedHeight = String(format: "%.2f", heightInFeet)
-            self.heightLabel.text = "\(formattedHeight)ft"
+    func displayMeasure(measure: String) {
+        
+        if let sizeInCm = Double(measure) {
+            if metricUnit {
+                if entity == .people {
+                    self.heightLabel.text = "\(measure)cm"
+                } else {
+                    self.heightLabel.text = "\(measure)m"
+                }
+            } else {
+                var heightInFeet = 0.0
+                if entity != .people {
+                    heightInFeet = sizeInCm*3.28084
+
+                } else {
+                    heightInFeet = sizeInCm*0.0328084
+                }
+
+                let formattedHeight = String(format: "%.2f", heightInFeet)
+                self.heightLabel.text = "\(formattedHeight)ft"
+            }
         }
     }
 }
